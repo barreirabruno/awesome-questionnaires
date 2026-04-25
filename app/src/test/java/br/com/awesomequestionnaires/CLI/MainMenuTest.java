64f4e7c41606607b520c410b43036517784bd6e7
barreiradev <barreiradev@192.168.0.26>
2026-04-24 23:20:21 -0300

    test:unit: ensure it parses general menu user option correctly

--- app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
+++ app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
@@ -1,27 +1,34 @@
 package br.com.awesomequestionnaires.CLI;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MainMenuTest {
 
+    Scanner scanner = new Scanner("1\n");
+
     @Test void ensureItCallsWelcomeMessageMethodSuccessfully() {
         String expected = "========================================\n" + "Greetings and welcome from Awesome Questionnaires.\n" + "========================================\n";
-        MainMenu classUndertest = new MainMenu();
+        MainMenu classUndertest = new MainMenu(scanner);
         assertNotNull(classUndertest.welcomeMessage(), expected);
     }
 
     @Test void ensureItBootsMainMenuSuccessfully() {
-        MainMenu menu = spy(new MainMenu());
+        MainMenu menu = spy(new MainMenu(scanner));
 
         menu.boot();
 
         verify(menu, times(1)).welcomeMessage();
+        verify(menu, times(1)).generalMenuOptions();
+        verify(menu, times(1)).assignGeneralMenuUserOption();
     }
 
     @Test void ensureItShowsUserOptionsSuccessfully() {
@@ -31,10 +38,12 @@ public class MainMenuTest {
                         "Type 3 for exit\n" + //
                         "\n" + //
                         "Please, type your option ";
-        MainMenu classUndertest = new MainMenu();
+        MainMenu classUndertest = new MainMenu(scanner);
         assertNotNull(classUndertest.generalMenuOptions(), expected);
     }
 
-    @Disabled
-    @Test void ensureItGetUserInputSuccessfully() {}
+    @Test void ensureItassignGeneralMenuUserOptionSuccessfully() {
+        MainMenu classUnderTest = new MainMenu(scanner);
+        assertEquals(classUnderTest.assignGeneralMenuUserOption(), "1");
+    }
 }
