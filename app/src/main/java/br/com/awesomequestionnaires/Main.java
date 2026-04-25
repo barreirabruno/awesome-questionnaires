793d6a69d0e0ddb45550c276c4827cba9f131137
barreiradev <barreiradev@192.168.0.26>
2026-04-24 22:18:54 -0300

    feat: use boot method on main

--- app/src/main/java/br/com/awesomequestionnaires/Main.java
+++ app/src/main/java/br/com/awesomequestionnaires/Main.java
@@ -3,12 +3,11 @@
  */
 package br.com.awesomequestionnaires;
 
-public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import br.com.awesomequestionnaires.CLI.MainMenu;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        MainMenu menu = new MainMenu();
+        menu.boot();
     }
 }
