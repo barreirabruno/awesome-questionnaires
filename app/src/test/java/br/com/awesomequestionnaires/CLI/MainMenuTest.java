e9b905233f1d21d999eb78383252f7c938a501bb
barreiradev <barreiradev@192.168.0.26>
2026-04-24 22:05:46 -0300

    test:unit: ensure it calls boot method successfully

--- app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
+++ app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
@@ -1,13 +1,25 @@
 package br.com.awesomequestionnaires.CLI;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 
 public class MainMenuTest {
+
     @Test void ensureItCallsWelcomeMessageMethodSuccessfully() {
         String expected = "========================================\n" + "Greetings and welcome from Awesome Questionnaires.\n" + "========================================\n";
         MainMenu classUndertest = new MainMenu();
         assertNotNull(classUndertest.welcomeMessage(), expected);
+    }
+
+    @Test void ensureItBootsMainMenuSuccessfully() {
+        MainMenu menu = spy(new MainMenu());
+
+        menu.boot();
+
+        verify(menu, times(1)).welcomeMessage();
     }
 }
