c533e63afb34c3c89b34e79883e6c5d2f2d9acb1
barreiradev <barreiradev@192.168.0.26>
2026-04-24 21:53:45 -0300

    feat: implements welcomeMessage method

---
+++ app/src/main/java/br/com/awesomequestionnaires/CLI/MainMenu.java
@@ -0,0 +1,9 @@
+package br.com.awesomequestionnaires.CLI;
+
+
+public class MainMenu {
+
+    public String welcomeMessage() {
+        return "========================================\n" + "Greetings and welcome from Awesome Questionnaires.\n" + "========================================\n";
+    }
+}
