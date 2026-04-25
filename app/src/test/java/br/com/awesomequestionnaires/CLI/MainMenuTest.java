0e80f20e88126597eced0d3d4ed25ff6652ba1a1
barreiradev <barreiradev@192.168.0.26>
2026-04-24 22:26:12 -0300

    test:unit: ensure it shows user options general menu successfully

--- app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
+++ app/src/test/java/br/com/awesomequestionnaires/CLI/MainMenuTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MainMenuTest {
@@ -22,4 +23,18 @@ public class MainMenuTest {
 
         verify(menu, times(1)).welcomeMessage();
     }
+
+        @Test void ensureItShowsUserOptionsSuccessfully() {
+        String expected = "Type 0 to create a new questionnaire\n" + //
+                        "Type 1 for manage questionnaires\n" + //
+                        "Type 2 to respond questionnaires\n" + //
+                        "Type 3 for exit\n" + //
+                        "\n" + //
+                        "Please, type your option ";
+        MainMenu classUndertest = new MainMenu();
+        assertNotNull(classUndertest.generalMenuOptions(), expected);
+    }
+
+    @Disabled
+    @Test void ensureItGetUserInputSuccessfully() {}
 }
