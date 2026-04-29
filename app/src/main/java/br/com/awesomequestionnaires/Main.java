bb0be0a38093fff40451d8fdf9f70e3e1d76b8b0
barreiradev <barreiradev@192.168.0.32>
2026-04-28 23:36:05 -0300

    refactor: inject scanner into MainMenu class

--- app/src/main/java/br/com/awesomequestionnaires/Main.java
+++ app/src/main/java/br/com/awesomequestionnaires/Main.java
@@ -3,11 +3,16 @@
  */
 package br.com.awesomequestionnaires;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import br.com.awesomequestionnaires.CLI.MainMenu;
 
 public class Main {
-    public static void main(String[] args) {
-        MainMenu menu = new MainMenu();
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        MainMenu menu = new MainMenu(scanner);
         menu.boot();
+        scanner.close();
     }
 }
